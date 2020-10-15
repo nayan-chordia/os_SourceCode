0c2116805543ff56a687e109cc90726468fe075b
Nayan Chordia <63711954+nayan-chordia@users.noreply.github.com>
2020-10-16 01:16:28 +0530

    Update scheduling_algorithm.cpp

--- scheduling_algorithm.cpp
+++ scheduling_algorithm.cpp
@@ -5,7 +5,18 @@ void FCFS(int arr[], int head , int m)
     int seek_count = 0;
     int distance, cur_track;
 
-    
+    for (int i = 0; i < m; i++) {
+        cur_track = arr[i];
+
+        // calculate absolute distance
+        distance = abs(cur_track - head);
+
+        // increase the total count
+        seek_count += distance;
+
+        // accessed track is now new head
+        head = cur_track;
+    }
 
     cout << "Total number of seek operations = "
         << seek_count << endl;
